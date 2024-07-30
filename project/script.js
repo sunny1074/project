423963a2f3c32bd9742c7afc11760902b04adccd
sunny1074 <palsunnykumar1074@gmail.com>
2024-07-30 23:22:29 +0530

    Add files via upload

--- project/script.js
+++ project/script.js
@@ -1,10 +1,20 @@
-const wrapper =document.querySelector('.wrapper');
-const loginlink =document.querySelector('.login-link');
-const registerlink =document.querySelector('.register-link');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
 
-registerlink.addEventListener('click', ()=>{
-    wrapper.classList.add('active');
-});
-loginlink.addEventListener('click', ()=>{
-    wrapper.classList.remove('active');
-});
+window.onscroll = () => { 
+    sections.foreach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.ClassList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+
+        };
+
+    });
+};
